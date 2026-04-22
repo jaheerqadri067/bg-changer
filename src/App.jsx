a104a8846711ff587716cae23b605eaa2f916284
jaheershaikh <jaheershaikh067@gmail.com>
2026-04-22 14:49:17 +0530

    add new file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Changer from './Changer'
+
+function App() {
+  return (
+    <>
+    {/* <h2 className='bg-black text-white text-center w-full h-11 cursor-pointer text-2xl pt-1'>Background Changer</h2> */}
+    <Changer />
+    </>
+  )
+}
+
+export default App
